7c1e02ee0bda6b4d60c53960210b0759373a737f
GrullonDev <prosystem155@gmail.com>
2025-04-24 00:53:20 -0600

    feat: add GraphQL API with file upload and static file serving
    Introduce GraphQL API with support for file uploads and static file serving. The schema includes queries and mutations for users and comments. The app now serves static files from the 'uploads' directory and supports file uploads with a maximum size of 10MB. Additionally, the app is configured to use PostgreSQL for data storage and Apollo Server for GraphQL.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
+import { UsuarioResolver } from './users/usuario.resolver';
+import { UsuarioService } from './users/usuario.service';
+import { ComentarioResolver } from './comments/comentario.resolver';
+import { ComentarioService } from './comments/comentario.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './users/usuario.entity';
+import { Comentario } from './comments/comentario.entity';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      csrfPrevention: false, // 👈 DESACTIVA CSRF
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'conexiones_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Usuario, Comentario]),
+  ],
+  providers: [
+    UsuarioResolver,
+    UsuarioService,
+    ComentarioResolver,
+    ComentarioService,
+  ],
 })
-export class AppModule {}
+
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { graphqlUploadExpress } from 'graphql-upload';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(graphqlUploadExpress({ maxFileSize: 10_000_000, maxFiles: 1 }));
+
+  // Servir imágenes
+  app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
+
+  await app.listen(3000);
 }
+
 bootstrap();
